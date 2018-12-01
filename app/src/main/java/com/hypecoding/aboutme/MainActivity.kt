87f6e94f3865d9bae806ff010528c76c4cb434dc
Milzam Hibatullah <hmilzam@gmail.com>
2018-12-01 13:02:47 +0700

    adding edittext,button, handle input

--- app/src/main/java/com/hypecoding/aboutme/MainActivity.kt
+++ app/src/main/java/com/hypecoding/aboutme/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.hypecoding.aboutme
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.done_button).setOnClickListener { adNickname(it) }
+    }
+
+    private fun adNickname(view: View) {
+        val nicknameEdit = findViewById<EditText>(R.id.name_edittext)
+        val nicknameText=findViewById<TextView>(R.id.nickname_text)
+        nicknameText.text=nicknameEdit.text
+        nicknameText.visibility=View.VISIBLE
+        nicknameEdit.visibility=View.GONE
+
+        //hide keyboard
+        val imm=getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken,0)
     }
 }
