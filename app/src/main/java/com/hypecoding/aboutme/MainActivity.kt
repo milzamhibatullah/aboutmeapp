30b8b6a6a5c050a9a9b16e8f6ae67da0c4c3411c
Milzam Hibatullah <hmilzam@gmail.com>
2018-12-02 10:23:58 +0700

    adding data binding

--- app/src/main/java/com/hypecoding/aboutme/MainActivity.kt
+++ app/src/main/java/com/hypecoding/aboutme/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.hypecoding.aboutme
 
 import android.content.Context
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,22 +9,26 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.hypecoding.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding : ActivityMainBinding
+    private val myName : MyName= MyName("Milzam Hibatullah")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.done_button).setOnClickListener { adNickname(it) }
+        binding =   DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.doneButton.setOnClickListener { adNickname(it) }
+        binding.myName=myName
     }
 
     private fun adNickname(view: View) {
-        val nicknameEdit = findViewById<EditText>(R.id.name_edittext)
-        val nicknameText=findViewById<TextView>(R.id.nickname_text)
-        nicknameText.text=nicknameEdit.text
-        nicknameText.visibility=View.VISIBLE
-        nicknameEdit.visibility=View.GONE
-
+        binding.apply {
+            myName?.nickname=nameEdittext.text.toString()
+            invalidateAll()
+            binding.nameEdittext.visibility=View.GONE
+            binding.doneButton.visibility=View.GONE
+            binding.nicknameText.visibility=View.VISIBLE
+        }
         //hide keyboard
         val imm=getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken,0)
